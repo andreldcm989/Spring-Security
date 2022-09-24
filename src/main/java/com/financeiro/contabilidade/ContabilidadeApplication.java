208ccf499d5b6a533961db9735042c5b013fadd3
Andre Cruz <andreldcruz@outlook.com>
2022-09-24 19:10:03 -0300

    Criação do projeto

---
+++ src/main/java/com/financeiro/contabilidade/ContabilidadeApplication.java
@@ -0,0 +1,13 @@
+package com.financeiro.contabilidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContabilidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContabilidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financeiro/contabilidade/ContabilidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financeiro.contabilidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContabilidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
