cf1c94c3033fa67eac3d27cbb74140c662076030
Andre Cruz <andreldcruz@outlook.com>
2022-09-27 13:04:45 -0300

    Criação da classe de configuração de segurança WebSecurity

--- src/main/java/com/financeiro/contabilidade/config/security/WebScurity.java
+++ src/main/java/com/financeiro/contabilidade/config/security/WebScurity.java
@@ -1,18 +1,30 @@
 package com.financeiro.contabilidade.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebScurity extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebScurity {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .httpBasic()
                 .and()
                 .authorizeHttpRequests()
-                .anyRequest().permitAll();
+                .anyRequest().authenticated()
+                .and().csrf().disable();
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
